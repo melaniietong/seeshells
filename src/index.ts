0328208454cf7e7f6d0bc925f5baff639a0b2e74
Melanie Tong <melaniietong@gmail.com>
2025-10-14 00:18:21 -0400

    add cors config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import type { Express } from 'express';
 import mainRouter from './routes/main.js';
+import cors from 'cors';
 
 const app: Express = express();
 const PORT: number = 9000;
+const CLIENT_PORT: number = 9001;
+
+app.use(cors({
+    origin: `http://localhost:${CLIENT_PORT}`,
+    methods: ['POST'],
+    allowedHeaders: ['Content-Type']
+  }));
 
 app.use('/', mainRouter);
 
