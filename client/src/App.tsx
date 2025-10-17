df7a86ad5da7697c7a3bf17823f7a800bd79f858
Melanie Tong <melaniietong@gmail.com>
2025-10-17 19:32:15 -0400

    create initial styling for main/upload view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,70 @@
-function App() {
+import { useEffect, useState } from 'react';
+import './styles/styles.scss'
+import Upload from './components/Upload.tsx';
+
+const App: React.FC = () => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  useEffect(() => {
+    const handleDragEnter = (e: DragEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDragging(true);
+    };
+
+    const handleDragLeave = (e: DragEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (e.relatedTarget === null) setIsDragging(false);
+    };
+
+    const handleDragOver = (e: DragEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    const handleDrop = (e: DragEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDragging(false);
+    };
+
+    window.addEventListener('dragenter', handleDragEnter);
+    window.addEventListener('dragleave', handleDragLeave);
+    window.addEventListener('dragover', handleDragOver);
+    window.addEventListener('drop', handleDrop);
+
+    return () => {
+      window.removeEventListener('dragenter', handleDragEnter);
+      window.removeEventListener('dragleave', handleDragLeave);
+      window.removeEventListener('dragover', handleDragOver);
+      window.removeEventListener('drop', handleDrop);
+    };
+  }, []);
+
+  const handleUpload = () => {
+    setIsDragging(false);
+  };
+
   return (
-    <div>Hi!</div>
+    <div className='main-container d-flex flex-center p-x-16'>
+      <div className={`drag-overlay ${isDragging ? 'visible' : ''}`} />
+
+      <div className='res-width-800 d-flex flex-col gap-32'>
+        <div className='w-100'>
+          <h1 className='title'>SeeShells</h1>
+          <div className='d-flex gap-8 justify-space-between'>
+            <div className='text-regular'>Identify seashells using AI image recognition</div>
+            <div className='text-regular'>🌊 🐚 👀 ✨</div>
+          </div>
+        </div>
+
+        <Upload 
+          isDragging={isDragging} 
+          onUpload={handleUpload} />
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/components/Upload.tsx
@@ -0,0 +1,66 @@
+import '../styles/styles.scss'
+import uploadIcon from '../assets/upload.svg'
+import { useRef } from 'react';
+
+interface UploadProps {
+    isDragging: boolean;
+    onUpload: () => void;
+}
+
+const Upload: React.FC<UploadProps> = ({ isDragging, onUpload }) => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleDrop = (e: React.DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const file = e.dataTransfer.files?.[0];
+        if (file) { onUpload(); }
+    };
+
+    const handleDragOver = (e: React.DragEvent) => {
+        e.preventDefault();
+    };
+
+    const handleInputClick = () => {
+        inputRef.current?.click();
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) { onUpload(); }
+    };
+
+    return (
+        <div className='upload-box w-100 background-gradient border-radius-25 p-32 res-height-500 '>
+            <div
+                className='h-100 d-flex flex-center flex-col gap-24 border-radius-25 dashed-border cursor-pointer'
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
+                onClick={handleInputClick}>
+                <img
+                    src={uploadIcon}
+                    alt='Upload'
+                    className={`w-80px upload-icon ${isDragging ? 'bounce' : ''}`} />
+
+                <p
+                    className='text-regular text-center'
+                    dangerouslySetInnerHTML={{
+                        __html: isDragging
+                            ? 'Drop your image here'
+                            : 'Drag and drop your seashell image here <br />or click to <u>browse your computer</u>',
+                    }} />
+
+                <input
+                    type='file'
+                    accept='image/png, image/jpeg, image/jpg'
+                    ref={inputRef}
+                    style={{ display: 'none' }}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Upload
